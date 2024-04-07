bc313910a12467744a25fa157cea781f9c9b6055
Rainer-seventeen <1652018592@qq.com>
2024-04-07 17:51:13 +0800

    first commit

---
+++ TODO.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author       : Rainer-seventeen 1652018592@qq.com
+ * @Date         : 2024-04-06 10:53:06
+ * @LastEditors  : Rainer-seventeen
+ * @LastEditTime : 2024-04-06 14:58:29
+ */
+
+// 导入YOLO模块，对图像识别(已完成)
+
+// TODO 实现python数据转化C++数据模块
+
+// TODO 完成录制视频的本地化追踪（使用本地化的图像识别）
+
+// TODO 参数标定，将图形与实际位置标定构成转化
+
+// TODO 衔接电控，完成本地化控制
+
+// TODO 调整接口，进一步完成实时流传输
+
+// TODO 配置时间参数，实时控制设计
---
+++ detecion/detect.cpp
@@ -0,0 +1,59 @@
+/*
+ * @Author       : Rainer-seventeen 1652018592@qq.com
+ * @Date         : 2024-04-07 17:13:56
+ * @LastEditors  : Rainer-seventeen
+ * @LastEditTime : 2024-04-07 17:35:23
+ */
+#include <iostream>
+#include <cstring>
+
+#include "opencv2/dnn/dnn.hpp"
+#include "opencv2/opencv.hpp"
+
+void Classification_good()
+{
+    // 装载模型，设置参数
+    // clock_t st = clock();
+    std::string model = "detecion/weights/best.onnx";
+
+    cv::dnn::ClassificationModel dnn_model(model);
+    dnn_model.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+    dnn_model.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
+
+    float scale = 1.0 / 255;
+    int inpWidth = 1920, inpHeight = 1080;
+    cv::Scalar mean(0, 0, 0);
+    dnn_model.setInputParams(scale, cv::Size(inpWidth, inpHeight), mean, true, false);
+
+    // clock_t end = clock();
+    // std::cout << end - st << std::endl;
+
+    // 图像文件夹遍历检测
+    std::string folder = "detecion/database";
+    std::vector<std::string> imagePathList;
+    cv::glob(folder, imagePathList);
+
+    std::cout << "test In C++!" << std::endl;
+    for (int i = 0; i < imagePathList.size(); i++)
+    {
+        cv::Mat img = cv::imread(imagePathList[i]);
+        // cv::resize(img, img, cv::Size(224, 224), 0, 0, cv::INTER_LANCZOS4);
+        cv::Mat img_t = cv::Mat::zeros(img.size(), CV_32FC1);
+
+        for (int ii = 0; ii < img.cols; ii++)
+        {
+            for (int jj = 0; jj < img.rows; jj++)
+            {
+                img_t.at<float>(ii, jj) = img.at<uchar>(ii, jj);
+            }
+        }
+
+        int classIds;
+        float confs;
+        double time1 = static_cast<double>(cv::getTickCount());
+        dnn_model.classify(img, classIds, confs); // 前向推理，classIds是类别索引,classIds=0是划痕，classIds=1是颗粒
+        double time2 = (static_cast<double>(cv::getTickCount()) - time1) / cv::getTickFrequency();
+        std::cout << classIds << std::endl;
+        std::cout << "time: " << time2 << std::endl;
+    }
+}
