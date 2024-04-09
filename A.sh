6d05070222936579571874682cd16e6df1e0c9b4
Rainer-seventeen <1652018592@qq.com>
2024-04-09 11:18:39 +0800

    增加删除上一次运行文件功能

--- A.sh
+++ A.sh
@@ -2,9 +2,10 @@
  # @Author       : Rainer-seventeen 1652018592@qq.com
  # @Date         : 2024-04-07 21:05:56
  # @LastEditors  : Rainer-seventeen
- # @LastEditTime : 2024-04-07 21:10:27
+ # @LastEditTime : 2024-04-09 10:41:16
 ### 
 
+rm -fr ./build
 cmake -B build
 make -C build/ -j4
 echo " "
