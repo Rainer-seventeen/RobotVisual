5cf5598339a2b620e9f5c60fcfe46513b09e29b8
Rainer-seventeen <1652018592@qq.com>
2024-04-09 11:19:03 +0800

    仅删除run，加快重编程速度

--- A.sh
+++ A.sh
@@ -2,10 +2,10 @@
  # @Author       : Rainer-seventeen 1652018592@qq.com
  # @Date         : 2024-04-07 21:05:56
  # @LastEditors  : Rainer-seventeen
- # @LastEditTime : 2024-04-09 10:41:16
+ # @LastEditTime : 2024-04-09 11:18:00
 ### 
 
-rm -fr ./build
+rm -fr ./build/run
 cmake -B build
 make -C build/ -j4
 echo " "
