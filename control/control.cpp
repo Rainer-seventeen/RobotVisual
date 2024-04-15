56f0c6fb8909d07e6af923f9cdf20aac5e4e41fc
Rainer-seventeen <1652018592@qq.com>
2024-04-15 20:28:31 +0800

    完成时间戳打印，实现时间戳

--- control/control.cpp
+++ control/control.cpp
@@ -2,7 +2,7 @@
  * @Author       : Rainer-seventeen 1652018592@qq.com
  * @Date         : 2024-04-14 20:06:04
  * @LastEditors  : Rainer-seventeen
- * @LastEditTime : 2024-04-14 21:29:51
+ * @LastEditTime : 2024-04-15 20:28:17
  */
 
 #include "control/control.hpp"
@@ -10,20 +10,24 @@
 void control::core()
 {
     std::cout << "Task: Control Model Opened!" << std::endl;
-    // 考虑将数据的输出放到这里来
+    // 考虑将数据的输出放到这里来（已经完成）
     // 先sub一下两个数据
+
     while (true)
     {
-        std::cout << "AAA" << std::endl;
+        // std::cout << "AAA" << std::endl; DEBUG
 
         umt::Subscriber<Detection_pack> detections_sub("detections_pack"); // 订阅传感器信息
-        const auto &[result, timestamp] = detections_sub.pop();
+        // std::cout << "BBB" << std::endl; DEBUG
+
+        const auto &[result, timestamp] = detections_sub.pop(); // 没有东西就一直处于挂起状态
+        std::time_t now = std::chrono::system_clock::to_time_t(timestamp);
+
+        std::cout << now << std::endl; // 打印时间戳（单位秒）
         int sz = result.size();
 
-        if (sz == 0)
-        {
-            std::cout << "ERROR: Subscribe Target Not found!" << std::endl;
-        }
+        // if (sz == 0)
+        //     std::cout << "ERROR: Subscribe Target Not found!" << std::endl; // 这个不会发生，因为如果为空不会publish
 
         for (int i = 0; i < sz; ++i)
         {
