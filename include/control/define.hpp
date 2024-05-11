0cd55a9722cc57b7a1752603167606c69a1278f3
Rainer-seventeen <1652018592@qq.com>
2024-05-11 21:47:45 +0800

    修改相机像素

--- include/control/define.hpp
+++ include/control/define.hpp
@@ -2,7 +2,7 @@
  * @Author       : Rainer-seventeen 1652018592@qq.com
  * @Date         : 2024-04-14 20:08:14
  * @LastEditors  : Rainer-seventeen
- * @LastEditTime : 2024-04-16 14:40:46
+ * @LastEditTime : 2024-05-11 21:47:37
  */
 
 // TODO:来实现判定是不是与上一个图标是同一个目标，并对物体进行标号
@@ -15,8 +15,8 @@
 const int MOVE_SPEED = 1000; // 假定的单位时间戳物体运动像素
 
 /*摄像机的参数*/
-const int CAMERA_X = 720;
-const int CAMERA_Y = 720;
+const int CAMERA_X = 640;
+const int CAMERA_Y = 480;
 
 namespace control
 {
